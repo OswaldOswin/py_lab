870a76296c1e2c275fe6e2709e4a43fc80aa57e1
Inessa Ivanova <42670922+spisladko@users.noreply.github.com>
2018-10-18 23:55:58 +0300

    Add files via upload

---
+++ lab_01/caesar.py
@@ -0,0 +1,54 @@
+def encrypt_caesar(plaintext: str) -> str:
+
+
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            if ord(plaintext[i]) in range(120, 123):
+                ciphertext += chr(ord(plaintext[i]) - 23)
+            elif ord(plaintext[i]) in range(88, 91):
+                ciphertext += chr(ord(plaintext[i]) - 23)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + 3)
+        else:
+            ciphertext += plaintext[i]
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str) -> str:
+
+
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+
+    plaintext = ''
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            if ord(ciphertext[i]) in range(97, 100):
+                plaintext += chr(ord(ciphertext[i]) + 23)
+            elif ord(ciphertext[i]) in range(65, 68):
+                plaintext += chr(ord(ciphertext[i]) + 23)
+            else:
+                plaintext += chr(ord(ciphertext[i]) - 3)
+        else:
+            plaintext += ciphertext[i]
+    return plaintext
